426869bea54f11a25eb5ced2340084ad3282713c
Trevis McGill <trevismcgill25@gmail.com>
2020-08-12 20:34:55 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,13 +1,15 @@
 # Write your code here
 class GetRequester
+  attr_accessor :url
 
 def initialize(url_address)
 
-url = url_address
+self.url = url_address
 end
 
 def get_response_body
-
+  response = Net::HTTP.get_response(uri)
+  response.body
 end
 
 def parse_json
